2c0c172e9a766eda6708486390e9ec66d7fa1a1e
ktkt9898 <kyletruschel@gmail.com>
2024-02-01 14:53:52 -0700

    Scaing notes

---
+++ Scaling_Notes.java
@@ -0,0 +1,9 @@
+/**
+ * Never have hard coded pixel values, incase of resizing.
+ * Use the width and height of the screen.
+ * Ex. frogDimension = 1/3*height (this acts as the anchor point)
+ * frogEyeDim = 1/3 * frogDimension
+ * Recall that an oval first makes a rectangle around it.
+ * frogY = (1/2 * height) - (1/2 * frogDim)
+ * (x0ffset, frogY)
+ */

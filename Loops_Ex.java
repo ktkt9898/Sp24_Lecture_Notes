1159369645deda8f25db8c46f2d1b8c8932e2c5e
ktkt9898 <kyletruschel@gmail.com>
2024-02-20 15:38:48 -0700

    While Loops notes

---
+++ Loops_Ex.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Loops_Ex {
+    public static void main(String[] args){
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Enter a non negative number ");
+        // int initialValue = 0;
+
+        // while (initialValue <= 0) {
+        //     System.out.print("Enter a non negative number ");
+        //     initialValue = input.nextInt();
+        //     System.out.println(initialValue);
+        // }
+        // System.out.println("Loop finished");
+
+        // input.close();
+
+        // int numTotal = 0;
+        // int numCount = 0;
+        
+        // while (numCount < 3) {
+        //     System.out.println("Enter number");
+        //     int num = input.nextInt();
+        //     numTotal = numTotal + num;
+        //     numCount++;
+        // }
+        // System.out.println(numTotal);
+        // input.close();
+
+        // // user an interator over a string
+        // Scanner strScan = new Scanner("The quick brown fox");
+
+        // // count number of words
+        // int wordsCount = 0;
+        // // while we have characters that have not been seen,
+        // // or else returns false
+        // while (strScan.hasNext()){
+        //     String nextWord = strScan.next();
+        //     wordsCount++;
+        // }
+        // System.out.println("There were " + wordsCount + " words");
+        // strScan.close();
+
+        // WIP
+//         int baseNum = 12;
+//         int innerCount = 0;
+
+//         while (baseNum <= 14) {
+//             System.out.println(baseNum + ":");
+//             while (innerCount <= 12) {
+//                 System.out.print(" " + innerCount * 12);
+//             }
+//             innerCount = 0;
+//             baseNum++;
+//         }
+//     }
+// }
+
---
+++ Nested_While_Loops.java
@@ -0,0 +1,42 @@
+/*
+ * While loops start with an initial condition and end with a
+ * terminal condition
+ * While loops continue until a condition is met
+ * For loops check for a given range
+ * Key elements:
+ * 
+ * Initial conditions, statements, looping condition
+ * 
+ * while (condition) {
+ *  statements if true
+ * }
+ * 
+ * terminating condition if statement is false
+ * Condition is checked once per iteration
+ * While loops should have some sort of update statement
+ * so the while condition returns false
+ * 
+ * While loops are best used when the iteration is not known
+ * A sentinel value is a value that will stop the loop
+ * 
+ * Loop tracing is understanding what the loop variables produce
+ * like loop count.
+ * 
+ * Iterator is an object that allows you to process a collection
+ * one item at a time.
+ * 
+ * Nested loops have loops inside another loop
+ * The inside loop will run before returning back to the outer loop
+ * limitX = 10
+ * LimitY = 5
+ * countX = 1
+ * count Y = 1
+ * 
+ * countY = 1 (this resets the count from 6 back to 1)
+ * countX++ (this increments the outer loop from 1 to 2)
+ * 1: 12345
+ * 2: 246810
+ * 3: ...
+ * 4: ...
+ * 10: 1020304050
+ */

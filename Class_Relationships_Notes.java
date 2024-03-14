fe5ba590c1e767e6a7369c8560477fb13bf79d44
ktkt9898 <kyletruschel@gmail.com>
2024-03-14 15:11:33 -0600

    Class Relationship Notes

---
+++ Class_Relationships_Notes.java
@@ -0,0 +1,32 @@
+/*
+ * To stub a method have its return be null;
+ * 
+ * Dependency (class uses)
+ * Aggregation (has a)
+ * Inheritance (Class is...)
+ * 
+ * Dependency: Class uses another class
+ * TaskMaster depends on ToDoist class "uses"
+ * 
+ * Aggregation: Class has a reference to another class
+ * typically with instance variables
+ * ToDoList is made up Tasks "has"
+ * 
+ * Every single class in java is an object
+ * Car class is a vehicle, but a SportsCar is a car with a bit of
+ * extra attributes
+ * The Code can be re-used and have more functionality (build on top
+ * of)
+ * TrafficAnimation extends JPanel
+ * JPanel allows drawing of painted components, but
+ * the TrafficAnimation allows us to have a entire scene/painting
+ * 
+ * This is why every class created has a toString method, even
+ * if it is not used so that a string representation can be
+ * printed of the object
+ * 
+ * Top down design starts with the driver class
+ * 
+ * Bottom up starts with the smaller class pieces with method
+ * stubs
+ */

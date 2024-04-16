6ae4565bea06813563959bbe36d3a9a678de43b7
ktkt9898 <kyletruschel@gmail.com>
2024-04-16 15:02:53 -0600

    GUI Notes

---
+++ GUI/GUI_Notes.java
@@ -0,0 +1,33 @@
+/**
+ * Event Driven Programming:
+ * Program waits for an event to happen, determined by user actions,
+ * then program responds
+ * 
+ * An EventListener is a method that responds to a event
+ * thru user action
+ * 
+ * JFrame is the outline/base of the GUI
+ * JPanel fills the frame and takes up space
+ * JLabel are text components
+ * JButton allows user action
+ * 
+ * Generally the size should correspond to the Frame or
+ * Panel to avoid dimension issues
+ * 
+ * JPanel is combined with the extends prefix, which allows
+ * inheritance/borrowing methods
+ * 
+ * ButtonListener should be a private class and have one
+ * method called actionPerformed (void)
+ * ActionEvent should be casted to the button/class name
+ * 
+ * 1. Create a panel to store components
+ * 2. Declare and instantiate components
+ * 3. Add action listeners to components as necessary
+ * 4. Add componenets to Panel
+ * 
+ * Panels can be stacked onto eachother. One panel
+ * can fill the role of being the main panel
+ * and then two panels can be filed inside to separate
+ * functions
+ */

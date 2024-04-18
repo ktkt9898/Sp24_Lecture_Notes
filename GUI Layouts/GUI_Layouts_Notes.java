1f6b3349ff32763af334db10fa46d89851519c99
ktkt9898 <kyletruschel@gmail.com>
2024-04-18 14:59:30 -0600

    GUI Layout notes

---
+++ GUI Layouts/GUI_Layouts_Notes.java
@@ -0,0 +1,14 @@
+/**
+ * BorderLayout allows North, South, West, East, and Center
+ * GridLayout is organized in a grid and will resize if the window is resized
+ * 
+ * A great way to organize a GUI is to layer panels
+ * 
+ * GUIs should be independent from backend, so
+ * the TicTacToe game should not depend on anything specific
+ * to the GUI
+ * 
+ * Layout must be set before more panels can be added
+ * 
+ * A BorderLayout can only have one component
+ */
